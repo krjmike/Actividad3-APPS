5828ce3af54aaf6240ad6bdb730eb26c72f41615
Maykel Joel Rhenals Julio <maykelrhenals_1@hotmail.com>
2023-11-19 23:48:31 -0500

    Formulario home

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -4,15 +4,51 @@ import 'package:fluttertoast/fluttertoast.dart';
 
 class HomeScreen extends StatelessWidget {
   final String username;
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
 
   HomeScreen(this.username);
 
   void _showToast(String message) {
     Fluttertoast.showToast(
       msg: message,
-      toastLength: Toast.LENGTH_SHORT,
+      toastLength: Toast.LENGTH_LONG,
       gravity: ToastGravity.BOTTOM,
     );
+  }
+
+  void _convertToMorse() {
+    String firstName = _firstNameController.text.toUpperCase();
+    String lastName = _lastNameController.text.toUpperCase();
+
+    String morseFirstName = _textToMorse(firstName);
+    String morseLastName = _textToMorse(lastName);
+
+
+    _showToast('Nombre en Morse: $morseFirstName');
+    _showToast('Apellido en Morse: $morseLastName');
+  }
+
+  String _textToMorse(String text) {
+    Map<String, String> morseCode = {
+      'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+      'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+      'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..',
+      '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+      '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----',
+      ' ': '/'
+    };
+
+    List<String> result = [];
+
+    for (int i = 0; i < text.length; i++) {
+      String char = text[i];
+      if (morseCode.containsKey(char)) {
+        result.add(morseCode[char]!);
+      }
+    }
+
+    return result.join(' ');
   }
 
   void _logout(BuildContext context) async {
@@ -30,8 +66,27 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Inicio - $username'),
       ),
-      body: Center(
-        child: Text('Bienvenido, $username'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _firstNameController,
+              decoration: InputDecoration(labelText: 'Nombre'),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              controller: _lastNameController,
+              decoration: InputDecoration(labelText: 'Apellido'),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: _convertToMorse,
+              child: Text('Convertir a Morse'),
+            ),
+          ],
+        ),
       ),
       drawer: Drawer(
         child: ListView(
@@ -52,7 +107,7 @@ class HomeScreen extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => _showToast('Realizado por Maykel Rhenals'),
-        child: Icon(Icons.add),
+        child: Icon(Icons.info),
       ),
     );
   }
